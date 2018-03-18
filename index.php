45fb694126be170ebac15cc2bb69ef050d790c20
Your Name <you@example.com>
2018-03-18 10:11:20 +0530

    Added cookie

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+$cookie_name = "count";
+$cookie_value = $_COOKIE[$cookie_name] || 20;
+$cookie_value = $cookie_value + 1;
+setcookie($cookie_name, $cookie_value, time() + (10 * 365 * 24 * 60 * 60), "/");
+header( 'Location: /index.html' );
+?>
